2e6fc2b2b53987b0527d5b052114e974bfe9521c
Poitier Stringer <pstringe@student.42.us.org>
2020-06-25 20:17:29 +0000

    added notes paddle movement

--- main.py
+++ main.py
@@ -9,42 +9,40 @@ tk.title('GAME')
 canvas = Canvas(tk, width=800, height=600)
 canvas.pack()
 
-##################################
+################################
 # Lesson 4 Objectives
 ## add go left and go_right methods
 ## add keybindings
-## define updat_postion funtion
+## define update_postion funtion
+################################
 
-
-##################################
-
-## Previous Lesson
-
-
-
-# 1. Define Paddle class
+# Define Paddle class
 class Paddle:
+  #initialization funciton called when new paddle is created
   def __init__(self, i_canvas):
     self.canvas = i_canvas
     self.id = canvas.create_rectangle(0, 0, 200, 10, fill='black')
     canvas.move(self.id, 200, 200)
+    
+    # 2. add keybindings to initialization function
+    
+  # 1. move goLeft and goRight methods into Paddle class
   
+  '''
+  def goLeft(e):
+      x = -10
 
-'''
-def goLeft(e):
-    x = -10
-
-
-def goRight(e):
-    x = 10
-'''
-# 2. Instanciate the paddle object
-paddle = Paddle(canvas)
+  def goRight(e):
+      x = 10
+  '''
+  # define update_position function
+  
+   
+paddle = Paddle(canvas) #create a paddle
 
 while True:
     tk.update_idletasks()
 
-    # 3. update the posisiton of the paddle object
+    # 4. update the posisiton of the paddle object
     
-    #tk.bind('<KeyPress-Down>', move)
     tk.update()
