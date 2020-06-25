04579927de02dfb5a8ba5c6d13f6f981af22c0ad
Poitier <poitier@Admins-MBP.attlocal.net>
2020-06-25 14:43:53 -0700

    finished keybinding.

--- main.py
+++ main.py
@@ -20,23 +20,25 @@ canvas.pack()
 class Paddle:
   #initialization funciton called when new paddle is created
   def __init__(self, i_canvas):
+    self.x = 0
     self.canvas = i_canvas
     self.id = canvas.create_rectangle(0, 0, 200, 10, fill='black')
-    canvas.move(self.id, 200, 200)
+    self.canvas.move(self.id, 200, 200)
     
     # 2. add keybindings to initialization function
-    self.canvas.bind_all('<KeyPress-Left>', self.goRight)
-    
+    self.canvas.bind_all('<KeyPress-Left>', self.goLeft)
+    self.canvas.bind_all('<KeyPress-Right>', self.goRight)
 
   # 1. move goLeft and goRight methods into Paddle class
-  def goLeft(e):
-      x = -10
+  def goLeft(self, e):
+      self.x = -10
 
-  def goRight(e):
-      x = 10
+  def goRight(self, e):
+      self.x = 10
 
   # define update_position function
-
+  def update_position(self):
+      self.canvas.move(self.id, self.x, 0)
 
 paddle = Paddle(canvas) #create a paddle
 
@@ -44,5 +46,5 @@ while True:
     tk.update_idletasks()
 
     # 4. update the posisiton of the paddle object
-    
+    paddle.update_position()    
     tk.update()
