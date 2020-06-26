ecda422375ba3aeeeb82e69c678b5c2d445ecfa7
Poitier <poitier@Admins-MBP.attlocal.net>
2020-06-26 08:05:59 -0700

    added right key binding

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import time
+
 tk = Tk()
 
 # change the game title
@@ -10,33 +11,35 @@ canvas = Canvas(tk, width=800, height=600)
 canvas.pack()
 
 ################################
-# Lesson 4 Objectives
-## add go left and go_right methods
-## add keybindings
-## define update_postion funtion
+# Lesson 5 Objectives
+## create boundries for the paddle
+## define ball class
+## make the ball move
 ################################
 
 # Define Paddle class
 class Paddle:
   #initialization funciton called when new paddle is created
   def __init__(self, i_canvas):
+    self.x = 0
     self.canvas = i_canvas
     self.id = canvas.create_rectangle(0, 0, 200, 10, fill='black')
     canvas.move(self.id, 200, 200)
+    # key bindings
+    self.canvas.bind_all('<KeyPress-Left>', self.goLeft)
+    self.canvas.bind_all('<KeyPress-Right>', self.goRight)
     
-    # 2. add keybindings to initialization function
     
-  # 1. move goLeft and goRight methods into Paddle class
   
-  '''
-  def goLeft(e):
-      x = -10
+  def goLeft(self, e):
+      self.x = -2
 
-  def goRight(e):
-      x = 10
-  '''
+  def goRight(self, e):
+      self.x = 2
+
   # define update_position function
-  
+  def update_position(self):
+      self.canvas.move(self.id, self.x, 0)
    
 paddle = Paddle(canvas) #create a paddle
 
@@ -44,5 +47,5 @@ while True:
     tk.update_idletasks()
 
     # 4. update the posisiton of the paddle object
-    
+    paddle.update_position()
     tk.update()
